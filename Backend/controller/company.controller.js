27db2ba6d9deacd63e881e0428859fd485bcf119
durgasahusahu <durgasahusahu1@gmail.com>
2025-04-27 13:11:40 +0530

    did changes in company controller

--- Backend/controller/company.controller.js
+++ Backend/controller/company.controller.js
@@ -1,6 +1,7 @@
 import { Company } from "../models/company.model.js";
 import { User } from "../models/user.model.js";
-import getDataUri from "../utils/dataURI.js";
+// import getDataUri from "../utils/dataURI.js";
+import getDataUri from "../utils/DataURI.js";
 import cloudinary from "../utils/cloudinary.js";
 
 
